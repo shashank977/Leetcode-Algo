d81fdd41e3a3e90a3e831a5a29faf8eaeb68b674
o__n <58528833+shashank977@users.noreply.github.com>
2020-06-25 16:39:38 +0530

    Create Binary_search.py

---
+++ Binary_search.py
@@ -0,0 +1,20 @@
+def search(nums, target):
+        left= 0
+        right= len(nums)-1
+        
+
+        
+        while left <= right:
+            middle= left + (right-left)//2
+            
+            
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right= middle-1
+            else:
+                left=middle+1
+        return -1
+            
+        # time:  O(log(n))      
+        # space: O(1)
