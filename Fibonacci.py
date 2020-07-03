ea08eb1bc775efee03fd49514cd452540f77ed9f
shashank977 <58528833+shashank977@users.noreply.github.com>
2020-07-04 03:09:27 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,25 +1,25 @@
-# (1)
+# (1) by memoization
+
 def fib(n,memoize={0:0,1:1,2:1}):
     if n in memoize:
         return memoize[n]
     else:
         memoize[n] = fib(n-1,memoize) + fib(n-2,memoize)
             return memoize[n]
-# time: o(n)    space: o(n)
+# time: O(n)    space: O(n)
 
 
 
 
-#(2)
-def fib(N):
-        
+#(2) by iteration
+
+def fib(N): 
     lastTwo=[0,1]
     counter=2
         
-    if N==0:
+    if N==0:        # base case
         return 0 
     
-        
     while counter <= N:
             
         nextFib = lastTwo[0]  + lastTwo[1]
@@ -27,3 +27,4 @@ def fib(N):
         lastTwo[1]=nextFib    
         counter+=1
     return lastTwo[1]
+# time: O(n)    space: O(1)
