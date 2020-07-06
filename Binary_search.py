9dd980bf3f794620bd14aafd24f1744bb4c0dc48
shashank977 <58528833+shashank977@users.noreply.github.com>
2020-07-06 15:27:49 +0530

    Update Binary_search.py

--- Binary_search.py
+++ Binary_search.py
@@ -1,19 +1,19 @@
 def search(nums, target):
-        left= 0
-        right= len(nums)-1
+    left= 0
+    right= len(nums)-1
         
 
         
-        while left <= right:
-            middle= (left + right)//2
+    while left <= right:
+        middle= (left + right)//2
             
             
-            if nums[middle] == target:
-                return middle
-            elif nums[middle] > target:
-                right= middle-1
-            else:
-                left=middle+1
+        if nums[middle] == target:
+            return middle
+        elif nums[middle] > target:
+            right= middle-1
+        else:
+            left=middle+1
         return -1
             
         # time:  O(log(n))      
