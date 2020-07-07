700e34e79fae546505c1dd797756f67346cff378
shashank977 <58528833+shashank977@users.noreply.github.com>
2020-07-07 15:53:02 +0530

    Update Is_Subsequence.py

--- Is_Subsequence.py
+++ Is_Subsequence.py
@@ -35,3 +35,19 @@ def isSubsequence(s, t):
             
     return False
     
+        
+        
+#(3)        
+def is_subsequence(s,t):
+    if s=="":
+        return True
+
+    s_pointer=0
+
+    for i in range(len(t)):
+        if t[i]==s[s_pointer]:
+            s_pointer+=1
+        if s_pointer==len(s):
+            return  True
+
+    return s_pointer==len(s)        
