11915a8be261f334bc40f900a53c576576c88ba0
shashank977 <58528833+shashank977@users.noreply.github.com>
2020-07-16 14:55:19 +0530

    Update Lucky_Integer_In_Array.py

--- Lucky_Integer_In_Array.py
+++ Lucky_Integer_In_Array.py
@@ -1,4 +1,10 @@
-from collections import Counter
+from collections import Counter                                         
 
-def findLucky(arr):
-  return max([j for i,j in Counter(arr).items() if i==j], default =-1)
+def findLucky(arr):                                                      
+  return max([j for i,j in Counter(arr).items() if i==j], default =-1)     
+
+
+
+# First I created a dictionary using Counter(arr)
+# Then converted that dictionary into a list with tuples of (i,j) using .items()
+# After that finding max, By default if the list is empty return -1.
