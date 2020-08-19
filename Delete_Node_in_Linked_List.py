61bc64f51d8f31af758780c711768938b1a1208f
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-08-19 20:01:17 +0530

    Update Delete_Node_in_Linked_List.py

--- Delete_Node_in_Linked_List.py
+++ Delete_Node_in_Linked_List.py
@@ -1,3 +1,9 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 def deleteNode(node):
   """
   :type node: ListNode
