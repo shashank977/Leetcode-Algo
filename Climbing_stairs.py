bd122629fe2491fbcc7cd12c76e9da83c5fa4782
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-08-27 23:18:51 +0530

    Create Climbing_stairs.py

---
+++ Climbing_stairs.py
@@ -0,0 +1,18 @@
+# using dynamic programming
+
+def climbStairs(self, n: int) -> int:
+    stairs = [1, 2]  
+    stepping = 3
+  if n == 0:
+    return 1
+  if n == 1:
+    return 1
+  if n == 2:
+    return 2
+        
+  while stepping <= n:
+    ways =  stairs[1] + stairs[0]
+    stairs[0] = stairs[1]
+    stairs[1] = ways
+    stepping +=1
+   return stairs[1]
