28f094d4e34c29e38e20d93a4f4c7644f76915c8
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-08-27 23:19:36 +0530

    Update Climbing_stairs.py

--- Climbing_stairs.py
+++ Climbing_stairs.py
@@ -3,16 +3,16 @@
 def climbStairs(self, n: int) -> int:
     stairs = [1, 2]  
     stepping = 3
-  if n == 0:
-    return 1
-  if n == 1:
-    return 1
-  if n == 2:
-    return 2
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
         
-  while stepping <= n:
-    ways =  stairs[1] + stairs[0]
-    stairs[0] = stairs[1]
-    stairs[1] = ways
-    stepping +=1
-   return stairs[1]
+    while stepping <= n:
+        ways =  stairs[1] + stairs[0]
+        stairs[0] = stairs[1]
+        stairs[1] = ways
+        stepping +=1
+    return stairs[1]
