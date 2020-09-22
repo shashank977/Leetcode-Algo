a5d3bf06a3018403878116f3396848d531e1efa9
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-09-23 01:05:55 +0530

    Update 121_Best_time_to_buy_&_sell_stock.py

--- 121_Best_time_to_buy_&_sell_stock.py
+++ 121_Best_time_to_buy_&_sell_stock.py
@@ -1,3 +1,17 @@
+# Brute force
+
+def maxProfit(stocks):
+    profit = 0
+    for i in range(len(stocks)-1):
+        for j in range(i+1, len(stocks)):
+            if stocks[j] - stocks[i] > profit:
+                max_profit = stocks[j] - stocks[i]
+    return profit
+
+
+
+
+
 def maxProfit(self, prices: List[int]) -> int:
     answer = 0
     smallest = float("inf")
