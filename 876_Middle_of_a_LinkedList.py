b9b0ab792e6015de0315e3db8f54fba17ff7236b
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-09-26 23:46:14 +0530

    Update 876_Middle_of_a_LinkedList.py

--- 876_Middle_of_a_LinkedList.py
+++ 876_Middle_of_a_LinkedList.py
@@ -2,9 +2,11 @@
 
 def middleNode(self, head):
   L = [head]
-  while L[-1].next:                   # Time: O(N)  ,  Space: O(N)
+  while L[-1].next:                   
     L.append(L[-1].next)
-  return L[len(L) // 2]
+  return L[len(L) // 2]        # Time: O(N)  ,  Space: O(N)
+
+
 
 
 
@@ -14,10 +16,9 @@ def middleNode(self, head):
   slow = head 
   fast = head
   
-  while fast.next:                    # Time: O(N)  ,  Space: O(1)
+  while fast.next:                    
     if not fast.next.next:    
       return slow.next
     slow = slow.next      
     fast = fast.next.next  
-  return slow
-                
+  return slow                  # Time: O(N)  ,  Space: O(1)
