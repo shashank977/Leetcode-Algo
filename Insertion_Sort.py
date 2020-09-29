9768361f127f9bdb1ba1ac4a9826d4ee480ff5a7
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-09-29 23:14:49 +0530

    Update Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,3 +1,6 @@
+#  Time:  O(N^2)   ,   Space:  O(1)
+
+#  (1)
 def insertion_sort(array):
     for item in range(1, len(array)):
         value_to_sort = array[item]
@@ -5,4 +8,18 @@ def insertion_sort(array):
             array[item] , array[item-1] = array[item-1], array[item]
             item = item - 1
     return array                       
-#   Time:  O(N^2)   ,   Space:  O(1)
+
+
+
+
+# (2) Using Swap function
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j-1]:
+            swap(j, j-1, array)
+            j -= 1
+    return array
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
