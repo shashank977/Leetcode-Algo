e5b2b74fac92fa3fde7348bb5da639132417cd1d
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-10-06 22:15:44 +0530

    Create Selection_sort.py

---
+++ Selection_sort.py
@@ -0,0 +1,9 @@
+def selection_sort(array):
+    for i in range(0 , len(array)-1):
+        min_value = i
+        
+        for j in range(i+1, len(array)):
+            if array[j] < array[min_value]:
+                min_value = j
+                array[min_value] , array[i] = array[i], array[min_value]
+    return array
