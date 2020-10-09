09113e45e7b1e2a31422fc06cb5198f0fa081ae7
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2020-10-10 00:44:04 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,12 @@
-def bubbleSort(array):
+def bubble(array):
     isSorted=False
 
     while not isSorted:
         isSorted=True
-        for i in range(len(array)-1):
+        for i in range(0, len(array)-1):
             if array[i] > array[i+1]:
                 array[i], array[i+1] = array[i+1], array[i]
-
                 isSorted=False
-
     return array
 
 
