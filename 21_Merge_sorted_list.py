dad00a237abb460f8f06b6dd978361247d228b68
Shashank Singh <58528833+shashank977@users.noreply.github.com>
2021-03-12 14:32:04 +0530

    Create 21_Merge_sorted_list.py

---
+++ 21_Merge_sorted_list.py
@@ -0,0 +1,22 @@
+def mergeTwoLists(self, l1, l2):
+  current = dummy = ListNode(0)
+  if not l1:
+    return l2
+  if not l2:
+    return l1
+    
+  while l1 and l2:
+    if l1.val < l2.val:
+      current.next = l1
+      l1 = l1.next
+    else:
+      current.next = l2
+      l2 = l2.next
+      current = current.next
+        
+  if l1:
+    current.next = l1
+  if l2:
+    current.next = l2
+        
+  return dummy.next
